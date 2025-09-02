f1c35a58dc6215c2b1f81ddc348e5e8faa56d1df
Galbapaz <gabriel.seap@hotmail.com>
2025-09-02 16:33:07 -0400

    new code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int x;
-        x = 5;
-        Console.WriteLine(x); 
+        Console.WriteLine("Introduce your age: "); 
+        string entrance = Console.ReadLine();
+        int age = int.Parse(entrance);
 
+        Console.WriteLine("your age is: " + age); 
     }
 }
