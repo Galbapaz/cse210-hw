3949bb4dadbe4767230331960d2620f15568bc5b
Galbapaz <gabriel.seap@hotmail.com>
2025-09-06 19:14:53 -0400

    this is exercise #1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        String name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        String Last_name = Console.ReadLine();
+
+        name = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+        Last_name = char.ToUpper(Last_name[0]) + Last_name.Substring(1).ToLower();
+        
+        
+        Console.WriteLine($"Your name is {Last_name}, {name} {Last_name}."); 
+
+
+
     }
 }
