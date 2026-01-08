9b73616b71fa9a48dd67e01a6a23e8dd280bac4b
Galbapaz <gabriel.seap@hotmail.com>
2026-01-08 11:45:14 -0700

    Core requirement # 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Diagnostics;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Console.WriteLine("What is the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine()); 
+
+        bool response = true; 
+
+        while (response == true) 
+        {
+          Console.WriteLine("What is your guess?"); 
+          int guessedNumber = int.Parse(Console.ReadLine()); 
+
+            
+             if (guessedNumber < magicNumber)
+            {
+                 Console.WriteLine("Higher");
+            }
+            else if (guessedNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            if (guessedNumber == magicNumber)
+            
+            {
+                response = false; 
+            }
+
+        }
+        Console.WriteLine(" You guessed it! " ); 
+           
+        
     }
 }
