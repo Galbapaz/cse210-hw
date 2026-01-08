38e9a3ae258beb91f5bba8c1f5b0072cc535e97a
Galbapaz <gabriel.seap@hotmail.com>
2026-01-08 12:25:50 -0700

    core requirements and stretch challenges, plus an additional function

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,41 +1,73 @@
 using System;
 using System.Diagnostics;
 using System.Net;
+using System.Runtime.CompilerServices;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
-    Random randomGenerator = new Random();
-    int magicNumber = randomGenerator.Next(1, 11); 
+        bool playAgain = true;
 
-        bool response = true; 
-
-        while (response == true) 
+        while (playAgain)
         {
-          Console.WriteLine("What is your guess?"); 
-          int guessedNumber = int.Parse(Console.ReadLine()); 
+            
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 11);
+
+            int attempts = 0;
+            bool guessing = true;
+
+            Console.WriteLine("I have chosen a magic number between 1 and 10. Can you guess it?");
 
             
-             if (guessedNumber < magicNumber)
+            while (guessing)
             {
-                 Console.WriteLine("Higher");
-            }
-            else if (guessedNumber > magicNumber)
-            {
-                Console.WriteLine("Lower");
+                Console.Write("Enter your guess: ");
+
+                
+                int guessedNumber;
+                while (!int.TryParse(Console.ReadLine(), out guessedNumber))
+                {
+                    Console.Write("Please enter a valid number: ");
+                }
+
+                attempts++;
+
+                if (guessedNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessedNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"The number of attempts was: {attempts}");
+                    guessing = false;
+                }
             }
 
-            if (guessedNumber == magicNumber)
             
+            string answer;
+            while (true)
             {
-                response = false; 
+                Console.Write("Do you want to play again? (yes/no): ");
+                answer = Console.ReadLine().ToLower().Trim();
+
+                if (answer == "yes" || answer == "no")
+                    break;
+                else
+                    Console.WriteLine("Please type 'yes' or 'no'.");
             }
 
+            playAgain = (answer == "yes");
         }
-        Console.WriteLine(" You guessed it! " ); 
-           
-        
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
+
