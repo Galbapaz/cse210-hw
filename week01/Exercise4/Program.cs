499228cece9269e62ceb444da29c5772421a38d8
Galbapaz <gabriel.seap@hotmail.com>
2026-01-08 21:37:50 -0700

    stretch challenges

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,59 +4,81 @@ using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
-    { 
+    {
+        var numbers = new List<int>();
 
-      var numbers = new List<int>{}; 
-
-      int sumTotal = 0; 
-      double average = 0; 
-      int maxNumber = 0; 
+        int sumTotal = 0;
+        double average = 0;
+        int maxNumber = 0;
+        int smallestPositive = int.MaxValue;
 
         int userNumber = -1;
 
-        while (userNumber != 0 )
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
-        {    Console.WriteLine("Enter a list of numbers,type 0 when finished: "); 
-             userNumber = int.Parse(Console.ReadLine());
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
 
             if (userNumber != 0)
             {
-              numbers.Add(userNumber);
+                numbers.Add(userNumber);
             }
-            
         }
 
-        foreach ( var number in numbers )
+       
+        foreach (var number in numbers)
         {
-            
-            sumTotal += number; 
+            sumTotal += number;
         }
-        
-        Console.WriteLine($"The sum of the numbers from the list is: {sumTotal}");  
 
+        Console.WriteLine($"The sum is: {sumTotal}");
 
         if (numbers.Count > 0)
         {
-          average = (double)sumTotal / numbers.Count;
-          Console.WriteLine($"The average is: {average}");
-        
+            
+            average = (double)sumTotal / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
 
-          for (int i = 0; i < numbers.Count; i++)
+            
+            maxNumber = numbers[0];
+            foreach (var number in numbers)
             {
-                if (numbers[i] > maxNumber)
+                if (number > maxNumber)
                 {
-                    maxNumber = numbers[i];
+                    maxNumber = number;
+                }
+            }
+            Console.WriteLine($"The largest number is: {maxNumber}");
+
+           
+            foreach (var number in numbers)
+            {
+                if (number > 0 && number < smallestPositive)
+                {
+                    smallestPositive = number;
                 }
             }
 
-            Console.WriteLine($"The largest number is: {maxNumber}");
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+            
+            numbers.Sort();
+
+            Console.WriteLine("The sorted list is:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
         }
         else
         {
             Console.WriteLine("No numbers were entered.");
         }
-        
-    
-
     }
 }
+
+    
+
