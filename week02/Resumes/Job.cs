c7aae0eea3a1be0fa72a37014ed9b7acdf501130
Galbapaz <gabriel.seap@hotmail.com>
2026-01-24 15:41:28 -0700

    changes

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,18 @@
+using System.Security.Cryptography;
+
+namespace Resumes
+{
+    public class Job
+    {
+        public string _jobTitle;
+        public  string _company;
+        public  int _startYear;
+        public  string _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}"); 
+        }
+
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using Resumes;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+            Job job1 = new Job();   // Create Object 
+
+            job1._jobTitle = "Teacher";
+            job1._company = "Thumim";
+            job1._startYear = 2025;
+            job1._endYear = "Present"; 
+
+            job1.Display(); 
+             
+
+            Console.ReadLine(); 
     }
 }
