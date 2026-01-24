131158f70faca9b8014ee17b969bd229d3c51aa8
Galbapaz <gabriel.seap@hotmail.com>
2026-01-24 15:50:48 -0700

    changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 using Resumes;
 
 
@@ -8,13 +9,23 @@ class Program
     static void Main(string[] args)
     {
             Job job1 = new Job();   // Create Object 
+            Job job2 = new Job(); 
 
             job1._jobTitle = "Teacher";
             job1._company = "Thumim";
             job1._startYear = 2025;
             job1._endYear = "Present"; 
 
+            job2._jobTitle = "Teacher";
+            job2._company = "Focus your mind";
+            job2._startYear = 2024;
+            job2._endYear = "December 2024"; 
+
+            
+
             job1.Display(); 
+
+            job2.Display(); 
              
 
             Console.ReadLine(); 
