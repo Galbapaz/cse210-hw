50f797747054e0f31b4bd0bc29d01f9d658a9ae1
Galbapaz <gabriel.seap@hotmail.com>
2026-02-18 21:48:05 -0700

    changes

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] parts = text.Split(' ');
+
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = _random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+
+        return displayText;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
